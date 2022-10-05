4ea1f1f9b0fbf61d60cf3a80c35125eb4791cfc8
vijeshnk <vijeshnirala@gmail.com>
2022-10-05 22:03:08 +0530

    align the cards

--- my-scope/product-feedback-app/pages/RoadMap.tsx
+++ my-scope/product-feedback-app/pages/RoadMap.tsx
@@ -3,17 +3,36 @@ import React from 'react'
 import RoadmapCard from '../components/RoadmapCard'
 import RoadmapNav from '../components/RoadmapNav'
 
+
+
+
 type Props = {}
 
 const RoadMap = (props: Props) => {
+  
   return (
     <>
     <RoadmapNav/>
-    <Grid container>
+    <Grid container spacing={4} sx={{paddingX:'100px', justifyContent:'center',marginTop:'100px'}}>
+      <Grid item  >
     <RoadmapCard/>
+    </Grid>
+    <Grid item >
+    <RoadmapCard/>
+    </Grid>
+    <Grid item >
+    <RoadmapCard/>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+    <RoadmapCard/>
+    </Grid>
     </Grid>
     </>
   )
 }
 
 export default RoadMap
+
+function makeStyles(arg0: { gridContainer: { paddingLeft: string; paddingRight: string } }): any {
+  throw new Error('Function not implemented.')
+}
