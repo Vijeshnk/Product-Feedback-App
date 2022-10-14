24b8088c1410d25568444620be2d1b9eb1b48db4
vijeshnk <vijeshnirala@gmail.com>
2022-10-14 17:12:22 +0530

    added characters count for comment text field

--- my-scope/product-feedback-app/components/AddCommentBox.tsx
+++ my-scope/product-feedback-app/components/AddCommentBox.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { Comment } from '@product_feedback/scope.textfield.comment';
-import { Card, Stack, Typography } from '@mui/material';
+import { Card, OutlinedInput, Stack, TextField, Typography } from '@mui/material';
 import { Appbutton } from '@product_feedback/scope.buttons.appbutton';
+
 
 type Props = {};
 
 const AddCommentBox = (props: Props) => {
+  const [input, setInput] = React.useState("");
+  const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
   return (
     <>
       <Card
@@ -32,7 +37,22 @@ const AddCommentBox = (props: Props) => {
           Add Comment
         </Typography>
         <div style={{ marginLeft: '4%', marginRight: '4%' }}>
-          <Comment placeholder="" />
+        <OutlinedInput
+        multiline
+        rows={3}
+            inputProps={{
+              maxLength: 225,
+            }}
+              
+              onChange={inputHandler}
+            sx={{
+              height: '10ch',
+              borderRadius: '6px',
+              color: 'hsl(231,33%,34%)',
+              bgcolor: 'hsl(230, 60%, 98%)',
+            }}
+            fullWidth
+          />
           <Stack direction="row" sx={{ mt: 3, pb: 5 }}>
             <Typography
               variant="subtitle1"
@@ -43,11 +63,12 @@ const AddCommentBox = (props: Props) => {
                 fontFamily: 'Jost',
               }}
             >
-              225 characters left
+            {225 - input.length}   characters left
             </Typography>
             <div style={{ position: 'absolute', right: '27%' }}>
               <Appbutton>Post Comment</Appbutton>
             </div>
+            
           </Stack>
         </div>
       </Card>
